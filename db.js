2395f2a4bd1a94454ec747a4938eeccf5af0e3c0
1251184-Robiul Hossain <rhrashal50@gmail.com>
2021-12-02 20:25:05 +0600

    update

--- db.js
+++ db.js
@@ -8,3 +8,15 @@ const connection = mysql.createConnection({
 });
 
 module.exports = connection;
+
+
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'mmohibur_mesuser',
+//   password: 'mW3)qapDT5sj',
+//   database: 'mmohibur_mesdb'
+// });
+
+// module.exports = connection;
